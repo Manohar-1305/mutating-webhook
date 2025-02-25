a1a877cfc709814918f77786929dbe6a5470a607
Manohar-1305 <maddy98989@gmail.com>
2025-02-25 21:39:12 +0530

    Update steps.sh

--- steps.sh
+++ steps.sh
@@ -19,3 +19,12 @@ openssl req -newkey rsa:4096 -nodes -keyout tls.key -x509 -days 365 -out tls.crt
 kubectl create secret tls webhook-tls --cert=webhook.crt --key=webhook.key -n default
 cat webhook.crt | base64 | tr -d '\n'
 
+-------------------------------------------
+openssl genrsa -out ca.key 2048
+openssl req -new -x509 -days 365 -key ca.key -subj "/C=CN/ST=GD/L=SZ/O=Acme, Inc./CN=Acme Root CA" -out ca.crt
+
+export SERVICE=webhook-service
+openssl req -newkey rsa:2048 -nodes -keyout tls.key -subj "/C=CN/ST=GD/L=SZ/O=Acme, Inc./CN=$SERVICE.default.svc.cluster.local" -out tls.csr
+openssl x509 -req -extfile <(printf "subjectAltName=DNS:$SERVICE.default.svc.cluster.local,DNS:$SERVICE.default.svc.cluster,DNS:$SERVICE.default.svc,DNS:$SERVICE.default.svc,DNS:$SERVICE.default,DNS:$SERVICE") -days 365 -in tls.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out tls.crt
+
+kubectl create secret tls tls --cert=tls.crt --key=tls.key
